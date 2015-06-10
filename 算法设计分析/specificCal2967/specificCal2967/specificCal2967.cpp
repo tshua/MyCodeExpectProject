8cb88038ac49f7a9e287905de9661e2c006f46db
田少华 <tshua.123@qq.com>
2015-06-10 14:16:44 +0800

    特殊日历2967.一次通过的感觉真爽.

---
+++ 算法设计分析/specificCal2967/specificCal2967/specificCal2967.cpp
@@ -0,0 +1,74 @@
+// specificCal2967.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+bool isRunYear(int year)
+{
+
+    if((year%4==0&&year%100!=0)||year%400==0)  
+        return true;  
+    else  
+        return false;
+
+}
+int calculateDay(int day,int month,int year)
+{
+	int count = 0;
+	int dayofmonth[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+	for(int i=2000;i<year;i++)
+	{
+		if(isRunYear(i))
+			count += 366;
+		else
+			count += 365;
+	}
+	if(isRunYear(year))
+		dayofmonth[1] = 29;
+	for(int i = 0;i<month-1;i++)
+	{
+		count += dayofmonth[i];
+	}
+	count+=day-1;
+	return count;
+}
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int n,hour,minute,second,day,month,year,mhour,mminute,msecond,mday,mmonth,myear;
+	long days,seconds;
+	cin>>n;
+	while(n--)
+	{
+		scanf("%d:%d:%d %d.%d.%d",&hour,&minute,&second,&day,&month,&year); 
+		//cout<<hour<<":"<<minute<<":"<<second<<" "<<day<<"."<<month<<"."<<year<<endl;
+		//cout<<calculateDay(day,month,year);
+		days = calculateDay(day,month,year);
+		myear =  days/1000;
+		days -=myear*1000;
+		mmonth = days/100+1;
+		days -= (mmonth-1)*100;
+		mday = days+1;
+		
+		//cout<<mday<<"  "<<mmonth<<" "<<myear<<endl;
+
+		seconds = hour*3600+minute*60+second;
+		seconds = seconds*1000/864;
+		cout<<seconds;
+
+		mhour = seconds/10000;
+		seconds -= 10000*mhour;
+		mminute = seconds/100;
+		seconds -= 100*mminute;
+		msecond =seconds;
+		//cout<<mhour<<"  "<<mminute<<" "<<msecond<<endl;
+
+		cout<<mhour<<":"<<mminute<<":"<<msecond<<" "<<mday<<"."<<mmonth<<"."<<myear<<endl;
+	}
+	
+
+
+	return 0;
+}
